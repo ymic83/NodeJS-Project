f21ce7687b89763239a2397b7a7c2368b004b861
ymic83 <ymic83@gmail.com>
2020-11-12 01:03:08 +0200

    add and remove favorites without page refresh

--- public/js/script.js
+++ public/js/script.js
@@ -1,37 +1,42 @@
-function user_like(movid, user) {
-    //attempting to send the the data via the add route to the movie.js file to update the database.
-
-    let response = fetch('/auth/add', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            id: movid.id
-        })
-
-    });
-    document.images[movid.id].src = "/img/like.png";
-    document.images[movid.id].alt = 'Click to unlike';
-}
-
-function user_unlike(movid, user) {
-    // attempting to send the the data via the remove route to the movie.js file to update the database.
-    let response = fetch('/auth/remove', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            id: movid.id
-        })
-
-    });
-    document.images[movid.id].src = "/img/unlike.png";
-    document.images[movid.id].alt = 'Click to like';
-}
-
-
 function non_user() {
     alert('Please register or login.')
+}
+
+
+
+function like(movid, user, button) {
+    //attempting to send the the data via the add route to the movie.js file to update the database.
+    debugger;
+    if (button == "tolike") {
+        let response = fetch('/auth/add', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                id: movid.id
+            })
+
+        });
+        debugger;
+        document.images[movid.id].src = "/img/like.png";
+        document.images[movid.id].alt = 'Click to unlike';
+        document.getElementById('tolike').id = 'tounlike';
+    } else {
+        let response = fetch('/auth/remove', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                id: movid.id
+            })
+
+        });
+        debugger;
+        document.images[movid.id].src = "/img/unlike.png";
+        document.images[movid.id].alt = 'Click to like';
+        document.getElementById('tounlike').id = 'tolike';
+        return;
+    }
 }
